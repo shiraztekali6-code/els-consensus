f04915b3f1308fe74e734b3605547f371fe6034e
shiraztekali6-code <shiraztekali6@gmail.com>
2026-02-22 00:09:35 +0200

    Update main.py

--- els-consensus/main.py
+++ els-consensus/main.py
@@ -1,26 +1,22 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import os
-
 from config.schema import QUESTION_SCHEMA
 
 app = FastAPI(title="ELS Consensus Annotation Server")
-
-# ---------- Static ----------
-app.mount("/images", StaticFiles(directory="images"), name="images")
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
-
 
 # ---------- API ----------
 @app.get("/schema")
 def get_schema():
     return QUESTION_SCHEMA
 
-
 @app.get("/images-list")
 def get_images_list():
-    files = sorted([
+    return sorted(
         f for f in os.listdir("images")
         if f.lower().endswith((".png", ".jpg", ".jpeg"))
-    ])
-    return files
+    )
+
+# ---------- Static ----------
+app.mount("/images", StaticFiles(directory="images"), name="images")
+app.mount("/ui", StaticFiles(directory="frontend", html=True), name="frontend")
